e92ebdb534f9f31b50d4313f71599ddd3b559215
helles43 <egetaha20113@gmail.com>
2024-12-01 20:35:11 +0300

    Update client.bat

--- client.bat
+++ client.bat
@@ -4,6 +4,33 @@ cd /d "%~dp0"
 title Essential-Things
 cls
 
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (e
+    echo This software needs administrative priviles to run certain apps and to confirm your software is legit and not knockoff...
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 :: Define the hardcoded local version of the script
 set "LOCAL_VERSION=14"  :: Change this version when you update the script
 set "URL=https://raw.githubusercontent.com/helles43/essential-things/main/client.bat"
